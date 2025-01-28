d6c942620d955bbba775302be7b1af14ac36cec8
jpecquais <48678018+jpecquais@users.noreply.github.com>
2025-01-28 15:34:57 +0100

    fix track filter packaging 3

--- Track Filter/jlp_Track filter.lua
+++ Track Filter/jlp_Track filter.lua
@@ -1,28 +1,6 @@
 -- @author JLP
 -- @version 25.01.27
 -- @description Track filter
--- @changelog
---     Initial release
--- @metapackage
--- @provides
---     [script main] .
---     [script main] jlp_Track filter - focus search bar.lua
---     [script main] jlp_Track filter - set selected track's name as track filters.lua
---     [script nomain] src/main_window.lua
---     [script nomain] src/main.lua
---     [script nomain] src/track_filter.lua
--- @about
---   # Track Filter
---   ## Description
---   The Track Filter script is a simple GUI based tool to show/hide tracks in MCP and TCP based on their name.
---   It requires my script **Visible tracks fit TCP height** to work !
---   **Note that you also need to install the common extension which also provides necessary functions.**
---   ## TODO
---   - [ ] Add options to affect TCP visiblity only
---   - [ ] Add options to affect MCP visiblity only
---   - [ ] Add options to show parent tracks of match tracks (currently on)
---   - [ ] Add options to show children tracks of match tracks (currently on)
---   # Known issue
 
 local main_path = reaper.GetResourcePath()
 local script_path = main_path .. "/Scripts/JLP Scripts"
