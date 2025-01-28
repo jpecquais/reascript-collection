4de1c722da5ba27f518ec6cbf0945a0d6c794db0
jpecquais <48678018+jpecquais@users.noreply.github.com>
2025-01-28 15:39:48 +0100

    try 7

--- Track Filter/jlp_Track filter.lua
+++ Track Filter/jlp_Track filter.lua
@@ -1,6 +1,7 @@
 -- @author JLP
 -- @version 25.01.27
 -- @description Track filter
+-- @noindex
 
 local main_path = reaper.GetResourcePath()
 local script_path = main_path .. "/Scripts/JLP Scripts"
